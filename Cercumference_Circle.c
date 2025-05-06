7bfce5ed8ae611177461fe651bfabf8f17c1e462
Raut Tejas <rauttejas2121@gmail.com>
2025-05-06 21:38:29 +0530

    Add files via upload

---
+++ Cercumference_Circle.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    float r,circumference;
+    printf("Enter the radius of the circle: ");
+    scanf("%f",&r);
+    circumference=2*3.14*r;
+    printf("Circumference of the circle is: %f",circumference);
+}
