29ee94c3b4bc15519e2596b4a9e16307c4631429
Raut Tejas <rauttejas2121@gmail.com>
2025-05-08 21:55:46 +0530

    Add files via upload

---
+++ Volume_Cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,volume;
+    printf("Enter the length of the side of the cube: ");
+    scanf("%d",&a);
+    volume=a*a*a;
+    printf("Volume of the cube is: %d",volume);
+    
+}
