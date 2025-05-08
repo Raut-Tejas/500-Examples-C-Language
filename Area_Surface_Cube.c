5ccd18ca3bace9a77b5d4859a02e99cd18b5d784
Raut Tejas <rauttejas2121@gmail.com>
2025-05-08 21:55:12 +0530

    Add files via upload

---
+++ Area_Surface_Cube.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,area;
+    printf("Enter the length of the side of the cube: ");   
+
+    scanf("%d",&a);
+    area=6*a*a;
+    printf("Surface area of the cube is: %d",area);
+}
