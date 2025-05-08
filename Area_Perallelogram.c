c19fc9e65052e415e585e5d332ea8aadafa0ea2d
Raut Tejas <rauttejas2121@gmail.com>
2025-05-08 21:53:20 +0530

    Add files via upload

---
+++ Area_Perallelogram.c
@@ -0,0 +1,11 @@
+#include<stdio.h>   
+void main()  
+{  
+    float b,h,area;  
+    printf("Enter the base of the parallelogram: ");  
+    scanf("%f",&b);  
+    printf("Enter the height of the parallelogram: ");  
+    scanf("%f",&h);  
+    area=b*h;  
+    printf("Area of the parallelogram is: %.2f",area);  
+}
