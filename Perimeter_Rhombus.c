f82cb25f87b84a673ed3cf4980ddd9767b5d3cb5
Raut Tejas <rauttejas2121@gmail.com>
2025-05-08 21:53:46 +0530

    Add files via upload

---
+++ Perimeter_Rhombus.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()  
+{  
+    float a,perimeter;  
+    printf("Enter the side of the rhombus: ");  
+    scanf("%f",&a);  
+    perimeter=4*a;  
+    printf("Perimeter of the rhombus is: %.2f",perimeter);  
+}
