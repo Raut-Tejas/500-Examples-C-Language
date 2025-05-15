102c841faedff27780e18ef5db4a510289bea651
Raut Tejas <rauttejas2121@gmail.com>
2025-05-15 21:18:33 +0530

    Delete Convert_Temperature_Fehrenheit.c

--- Convert_Temperature_Fehrenheit.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-void main(){
-    float c,f;
-    printf("Enter temperature in Celsius: ");
-    scanf("%f",&c);
-    f=(c*9/5)+32;
-    printf("Temperature in Fahrenheit is: %.2f",f);
-}
